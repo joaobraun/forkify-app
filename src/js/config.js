3ce516110c064020ebb48f950cebfb2e6d90f171
unknown <joaoalbertobraun@gmail.com>
2024-02-19 18:14:41 -0300

    add recipe feature

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '83ad37f5-d909-41a7-ad62-23d3332a781a';
+export const MODAL_CLOSE_SEC = 1;
